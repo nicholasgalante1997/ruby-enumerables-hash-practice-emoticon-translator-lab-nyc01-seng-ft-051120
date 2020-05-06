6b072af600efe2f51162f5283e5ac226d43859c6
Nicholas Galante <ngalant2@binghamton.edu>
2020-05-06 15:13:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,19 @@
-# require modules here
+require "yaml"
+require "pry"
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  emoticons = YAML.load_file(file_path)
+  return_hash = {
+    "get_meaning" => {},
+    "get_emoticon" => {},
+  }
+  emoticons.each do |meaning, translations|
+    return_hash["get_meaning"][translations[1]] = meaning
+    return_hash["get_emoticon"][translations[0]] = translations[1]
+  end
+  return_hash
+end
+ 
 end
 
 def get_japanese_emoticon
